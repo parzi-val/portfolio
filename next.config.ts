b5ac0cc44dafe2b27585e77b88ee2187a3b1e96c
KR Bala <krbala1511@gmail.com>
2025-03-22 22:05:07 +0530

    Revert "more changes"
    This reverts commit bd9acf4dab2bee7d11dd9281c46e1816bdd42b28.

--- next.config.ts
+++ next.config.ts
@@ -1,29 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Disable webpack cache to prevent memory issues
-  webpack: (config, { dev, isServer }) => {
-    // Force filesystem cache off
-    config.cache = false;
-    
-    // Optimize memory usage
-    config.optimization = {
-      ...config.optimization,
-      minimize: true,
-    };
-    
-    return config;
-  },
-  
-  // Reduce image optimization pressure during build
-  images: {
-    unoptimized: process.env.NODE_ENV !== 'production',
-  },
-  
-  // Use supported experimental features only
-  experimental: {
-    // Remove unsupported property
-  }
+  /* config options here */
 };
 
 export default nextConfig;
